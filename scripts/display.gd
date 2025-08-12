6517695309c469326fd607a35071bd0503d9cb86
chico <franciscoribeirodemelo2008@gmail.com>
2025-08-12 20:39:14 -0300

    ajustes visuais minimos
    os displays ligam / desligam de acordo com o estado do botao de ligar e desliga
    agora o potenciometro selecionado tem uma borda amarela

--- scripts/display.gd
+++ scripts/display.gd
@@ -1,5 +1,8 @@
 extends Node
 
+const COR_ON = Color(1, 0, 0, 1)
+const COR_OFF = Color(0, 0, 0, 0)
+
 @onready var pot_voltagem1 = $"../potenciometros/pot_voltagem1"
 @onready var pot_voltagem2 = $"../potenciometros/pot_voltagem2"
 @onready var pot_corrente1 = $"../potenciometros/pot_corrente1"
@@ -9,7 +12,14 @@ extends Node
 @onready var display_voltagem2 = $voltagem2
 @onready var display_corrente1 = $corrente1
 @onready var display_corrente2 = $corrente2
-	
+
+var tween: Tween
+
+func _ready() -> void:
+	display_voltagem1.modulate = COR_OFF
+	display_voltagem2.modulate = COR_OFF
+	display_corrente1.modulate = COR_OFF
+	display_corrente2.modulate = COR_OFF
 
 func _on_pot_voltagem_1_saida_alterada(saida: Variant) -> void:
 	escrever_display(display_voltagem1, saida)
@@ -25,3 +35,24 @@ func _on_pot_corrente_2_saida_alterada(saida: Variant) -> void:
 
 func escrever_display(display, n) -> void:
 	display.text = "%05.2f" % n
+
+func _on_botao_on_off_toggled(toggled_on: bool) -> void:
+	textoVisivel(toggled_on)
+	
+func textoVisivel(on) -> void:
+	var cor
+	if on:
+		cor = COR_ON
+	else:
+		cor = COR_OFF
+	
+	if tween:
+		tween.kill()
+	
+	tween = create_tween()
+	tween.set_parallel(true)
+	
+	tween.tween_property(display_voltagem1, "modulate", cor, 0.2).set_trans(Tween.TRANS_CIRC)
+	tween.tween_property(display_voltagem2, "modulate", cor, 0.2).set_trans(Tween.TRANS_CIRC)
+	tween.tween_property(display_corrente1, "modulate", cor, 0.2).set_trans(Tween.TRANS_CIRC)
+	tween.tween_property(display_corrente2, "modulate", cor, 0.2).set_trans(Tween.TRANS_CIRC)
--- scripts/potenciometros.gd
+++ scripts/potenciometros.gd
@@ -1,6 +1,7 @@
 extends Node
 
 @onready var pot_rotacao = $potenciometro_rotacao
+@onready var sprite_selecao = $sprite_selecao
 @onready var pot_voltagem1 = $pot_voltagem1
 @onready var pot_voltagem2 = $pot_voltagem2
 @onready var pot_corrente1 = $pot_corrente1
@@ -33,21 +34,27 @@ func selecionar(pot: potenciometro_click) -> void:
 	if selecionado != pot:
 		selecionado = pot
 		pot_rotacao.rotation = selecionado.rotation
+		sprite_selecao.position = selecionado.position + selecionado.pivot_offset
 	else:
 		selecionado = null
+		
+	var visivel = selecionado != null
 	
-	pot_rotacao.visible = selecionado != null
+	sprite_selecao.visible = visivel
+	pot_rotacao.visible = visivel
 
 func _on_potenciometro_rotacao_rotacionado(rotacao: float) -> void:
 	if !selecionado: return
 	var valor_saida_max = selecionado.VALOR_MAXIMO
+	
 	var rot_max = pot_rotacao.ANGULO_MAX
 	var rot_min = pot_rotacao.ANGULO_MIN
+	
 	var saida = remap(rotacao, rot_min, rot_max, 0, valor_saida_max)
+	
 	saida = clamp(saida, 0, valor_saida_max)
 	saida = floor(saida*100)/100
 	
 	selecionado.rotation = rotacao
 	selecionado.saida = saida
 	selecionado.saida_alterada.emit(saida)
-	#print(floor(saida*100)/100)
