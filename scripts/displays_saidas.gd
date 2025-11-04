fb6a60ecb3bbf8cd1b52c91495a886b1632dc8ad
chico <franciscoribeirodemelo2008@gmail.com>
2025-11-03 22:57:17 -0300

    implementação basica do modo indep

--- scripts/displays_saidas.gd
+++ scripts/displays_saidas.gd
@@ -11,6 +11,9 @@ const PONTO_B = enums.SAIDAS.POS_1
 const PONTO_C = enums.SAIDAS.NEG_1
 const PONTO_D = enums.SAIDAS.NEG_2
 
+var grafo = {}
+var V_indep = {}
+
 func get_primeiro_comum() -> int:
 	var comuns = hitboxes.get_comuns()
 	if comuns.size() <= 0:
@@ -18,55 +21,33 @@ func get_primeiro_comum() -> int:
 	
 	return comuns[0]
 
-#func modo_serie() -> void:
-#	var v1 = pot_voltagem2.saida
-#	var v2 = pot_voltagem2.saida
-#	var ponto_comum = get_primeiro_comum()
-#	if ponto_comum == -1:
-#		ponto_comum = PONTO_C
-#	
-#	var potenciais = {
-#		PONTO_A: v1 + v2,
-#		PONTO_B: v1,
-#		PONTO_C: 0,
-#		PONTO_D: v1,
-#	}
-#	
-#	var tensao_comum = potenciais[ponto_comum]
-#	var va = potenciais[PONTO_A] - tensao_comum
-#	var vb = potenciais[PONTO_B] - tensao_comum
-#	var vc = potenciais[PONTO_C] - tensao_comum
-#	var vd = potenciais[PONTO_D] - tensao_comum
-#	
-#	$d1.text = str(vb)
-#	$d3.text = str(vc)
-#	$d4.text = str(va)
-#	$d6.text = str(vd)
-#
-#func modo_paralelo() -> void:
-#	var v1 = pot_voltagem2.saida
-#	var v2 = pot_voltagem2.saida
-#	var ponto_comum = get_primeiro_comum()
-#	if ponto_comum == -1:
-#		ponto_comum = PONTO_C
-#	
-#	var potenciais = {
-#		PONTO_A: v2,
-#		PONTO_B: v1,
-#		PONTO_C: 0,
-#		PONTO_D: 0,
-#	}
-#	
-#	var tensao_comum = potenciais[ponto_comum]
-#	var va = potenciais[PONTO_A] - tensao_comum
-#	var vb = potenciais[PONTO_B] - tensao_comum
-#	var vc = potenciais[PONTO_C] - tensao_comum
-#	var vd = potenciais[PONTO_D] - tensao_comum
-#	
-#	$d1.text = str(vb)
-#	$d3.text = str(vc)
-#	$d4.text = str(va)
-#	$d6.text = str(vd)
+func criar_permutacao(fio) -> Array:
+	var permutacao = []
+	permutacao.append(fio[1])
+	permutacao.append(fio[0])
+	return permutacao
+	
+func recebe_tensao_origem_destino(origem, destino):
+	for key in grafo:
+		if key == [origem, destino]:
+			return grafo[key]
+			
+func recebe_vizinhos(no):
+	var vizinhos = []
+	for key in grafo:
+		if key[0] == no:
+			vizinhos.append(key[1])
+	return vizinhos
+	
+func percorre(atual, origem):
+	if origem:
+		V_indep[atual] = V_indep[origem] + recebe_tensao_origem_destino(origem, atual)
+	var vizinhos = recebe_vizinhos(atual)
+	if origem in vizinhos:
+		vizinhos.erase(origem)
+		
+	for vizinho in vizinhos:
+		percorre(vizinho, atual)
 
 func modo_serie_paralelo() -> void:
 	var v1 = pot_voltagem2.saida
@@ -106,15 +87,44 @@ func modo_serie_paralelo() -> void:
 	$d4.text = str(va)
 	$d6.text = str(vd)
 
+func modo_indep() -> void:
+	var v1 = pot_voltagem1.saida
+	var v2 = pot_voltagem2.saida
+	
+	grafo = {
+		[PONTO_C, PONTO_A]: v1,
+		[PONTO_D, PONTO_B]: v2,
+		[PONTO_A, PONTO_C]: -v1,
+		[PONTO_B, PONTO_D]: -v2,
+	}
+	
+	for fio in hitboxes.fios_na_fonte:
+		grafo[fio] = 0
+		grafo[criar_permutacao(fio)] = 0
+	
+	V_indep = {
+		PONTO_A: 0,
+		PONTO_B: 0,
+		PONTO_C: 0,
+		PONTO_D: 0,
+	}
+	
+	percorre(PONTO_B, null)
+	$d1.text = str(V_indep[PONTO_B])
+	$d3.text = str(V_indep[PONTO_C])
+	$d4.text = str(V_indep[PONTO_A])
+	$d6.text = str(V_indep[PONTO_D])
+
 # melhorar depois pfv
 # usa um match ai pls
 func _process(f) -> void:
-	modo_serie_paralelo()
-	if seletora.estado == enums.ESTADOS_FONTE.INDEP:
-		$d1.text = "?"
-		$d3.text = "?"
-		$d4.text = "?"
-		$d6.text = "?"
+	match seletora.estado:
+		enums.ESTADOS_FONTE.INDEP:
+			modo_indep()
+		enums.ESTADOS_FONTE.SERIES:
+			modo_serie_paralelo()
+		enums.ESTADOS_FONTE.PARALELL:
+			modo_serie_paralelo()
 	
 	if $"../botao_on_off".button_pressed:
 		$d1.visible = true
